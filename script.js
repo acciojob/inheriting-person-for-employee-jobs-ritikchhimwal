d121cf2d5a22bda167d02e217a3db781d7650491
Ritik Chhimwal <132690630+ritikchhimwal@users.noreply.github.com>
2024-11-19 19:32:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,34 @@
 // complete this js code
-function Person(name, age) {}
+// Define the Person class
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+}
 
-function Employee(name, age, jobTitle) {}
+// Add the greet method to the Person prototype
+Person.prototype.greet = function () {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+};
+
+// Define the Employee class
+function Employee(name, age, jobTitle) {
+    // Call the Person constructor with the name and age parameters
+    Person.call(this, name, age);
+    this.jobTitle = jobTitle;
+}
+
+// Set up inheritance from Person
+Employee.prototype = Object.create(Person.prototype);
+
+// Restore the constructor reference for Employee
+Employee.prototype.constructor = Employee;
+
+// Add the jobGreet method to the Employee prototype
+Employee.prototype.jobGreet = function () {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+};
 
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
+
